a0893c7dc21b420f6251214d9612141abe563d8b
Marissa Boyd <mboyd47@students.kennesaw.edu>
2021-10-08 16:44:38 -0400

    added population dataset

--- scripts/data_munging.py
+++ scripts/data_munging.py
@@ -5,9 +5,15 @@ import altair as alt
 # %%
 #importing data
 ebola = pd.read_csv("/Users/marissaboyd/Downloads/ebola_data_db_format.csv")
+world_pop = pd.read_csv("/Users/marissaboyd/Downloads/population-figures-by-country-csv_csv.csv")
 # %%
 #saved data to data folder
 #ebola.to_csv("/Users/marissaboyd/git/data-visualization-boyd-python/data/ebola.csv")
+#world_pop.to_csv("/Users/marissaboyd/git/data-visualization-boyd-python/data/world_pop.csv")
+#%%
+world_pop_2014 = pd.DataFrame(world_pop['Year_2014'])
+world_pop_2015 = pd.DataFrame(world_pop['Year_2015'])
+countries = pd.DataFrame(world_pop['Country'])
 # %%
 ebola['Date'] = pd.to_datetime(ebola.Date)
 ebola = ebola[ebola.Date != '2016-03-23']
